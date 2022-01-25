7028a610406eeafc34a42534a38477e02b0b5cdf
takacsberni <takacsberni@gmail.com>
2022-01-25 14:52:14 +0100

    feat: layout.html now contains css and bootstrap codes from design.css

--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -17,8 +17,30 @@ export function htmlFactory(template) {
 
 function boardBuilder(board) {
     return `<div class="board-container">
-                <div class="board" data-board-id=${board.id}>${board.title}</div>
-                <button class="toggle-board-button" data-board-id="${board.id}">Show Cards</button>
+                <section class="board" data-board-id=${board.id}>${board.title}</section>
+                <div class="board-header"><span class="board-title">Board 1</span>
+                    <button class="board-add">Add Card</button>
+                    <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
+                    <button class="toggle-board-button" data-board-id="${board.id}">Show Cards</button>
+                </div>
+                <div class="board-columns">
+                <div class="board-column">
+                    <div class="board-column-title">New</div>
+                    <div class="board-column-content">
+                </div>
+                <div class="board-column">
+                    <div class="board-column-title">In progress</div>
+                    <div class="board-column-content">
+                </div>
+                <div class="board-column">
+                    <div class="board-column-title">Testing</div>
+                    <div class="board-column-content">
+                </div>
+                <div class="board-column">
+                    <div class="board-column-title">Done</div>
+                    <div class="board-column-content">
+                </div>
+                
             </div>`;
 }
 
